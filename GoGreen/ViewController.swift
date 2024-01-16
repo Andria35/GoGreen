7efd836eb4139f1fc6ab106bd6885f4b4e7ab991
Andria <andria.inas@gmail.com>
2024-01-16 13:22:12 +0400

    first commit

---
+++ GoGreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoGreen
+//
+//  Created by Andria Inasaridze on 16.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
