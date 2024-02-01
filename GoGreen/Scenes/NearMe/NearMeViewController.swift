0826dbb1ac42be7cce4967812c36a48dd9c160a3
Andria <andria.inas@gmail.com>
2024-02-01 17:00:32 +0400

    refactor add-near-me-tab branch, before merging with main branch

--- GoGreen/Scenes/NearMe/NearMeViewController.swift
+++ GoGreen/Scenes/NearMe/NearMeViewController.swift
@@ -8,55 +8,65 @@
 import UIKit
 import MapKit
 
-final class NearMeViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
-
+final class NearMeViewController: UIViewController {
+    
     // MARK: - Class Properties
     let locationManager = CLLocationManager()
-    var shouldFollowUserLocation = true
-
+    
     // MARK: - UI Components
-    private let mapView = MKMapView()
-
+    let mapView = MKMapView()
+    
     
     // MARK: - ViewLifeCycles
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupUI()
+        setupConstraints()
+    }
+    
+    
+    // MARK: - Setup UI
+    private func setupUI() {
+        setupSubViews()
+        setupLocationManager()
         setupMapView()
-        
-        // Request location permissions
+    }
+    
+    private func setupSubViews() {
+        view.addSubview(mapView)
+    }
+    
+    private func setupLocationManager() {
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
-        
-        // Set up MapView
+    }
+    
+    private func setupMapView() {
         mapView.delegate = self
         mapView.showsUserLocation = true
     }
     
-    // CLLocationManagerDelegate method to handle location updates
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let userLocation = locations.first?.coordinate {
-            // Set the region to show the user's location only if shouldFollowUserLocation is true
-//            if shouldFollowUserLocation {
-//                let region = MKCoordinateRegion(center: userLocation, latitudinalMeters: 1000, longitudinalMeters: 1000)
-//                mapView.setRegion(region, animated: true)
-//            }
-            let region = MKCoordinateRegion(center: userLocation, latitudinalMeters: 1000, longitudinalMeters: 1000)
-            mapView.setRegion(region, animated: true)
-            
-            // Perform a search for plant stores
-            searchForPlantStores(near: userLocation)
-        }
+    // MARK: - Setup Constraints
+    private func setupConstraints() {
+        setupMapViewConstraints()
     }
     
+    private func setupMapViewConstraints() {
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+    
+    // MARK: - Class Methods
     func searchForPlantStores(near location: CLLocationCoordinate2D) {
-        // Create a search request
         let request = MKLocalSearch.Request()
         request.naturalLanguageQuery = "plant shop"
         request.region = mapView.region
         
-        // Perform the search
         let search = MKLocalSearch(request: request)
         search.start { (response, error) in
             guard let response = response, error == nil else {
@@ -64,13 +74,9 @@ final class NearMeViewController: UIViewController, MKMapViewDelegate, CLLocatio
                 return
             }
             
-            // Remove previous annotations
             self.mapView.removeAnnotations(self.mapView.annotations)
             
-            // Process the search results
             for item in response.mapItems {
-                // You can access information about each result using item.name, item.placemark, etc.
-                // Here, we add an annotation for each result on the map.
                 let annotation = MKPointAnnotation()
                 annotation.coordinate = item.placemark.coordinate
                 annotation.title = item.name
@@ -78,13 +84,7 @@ final class NearMeViewController: UIViewController, MKMapViewDelegate, CLLocatio
             }
         }
     }
-        
-    func setupMapView() {
-        view.addSubview(mapView)
-        
-        mapView.translatesAutoresizingMaskIntoConstraints = false
-        mapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-    }}
+
+    
+}
+
