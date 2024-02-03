95bcd3f5fee8d68200bc8e022128056be9142ad5
Andria <andria.inas@gmail.com>
2024-02-03 19:04:48 +0400

    add UIViewController extension to add dismiss Keyboard functionality, when screen is tapped and implement it in AddNewMyPlantView

--- GoGreen/Scenes/MyGarden/AddNewMyPlantView/AddNewMyPlantViewController.swift
+++ GoGreen/Scenes/MyGarden/AddNewMyPlantView/AddNewMyPlantViewController.swift
@@ -63,6 +63,7 @@ final class AddNewMyPlantViewController: UIViewController {
         setupUI()
         setupConstraints()
         setupDelegates()
+        setupToHideKeyboardOnTapOnView()
     }
     
     // MARK: - Initialization
